4d5db3a8963b52fe8cf42b9e2e81c9153975ab92
Graham Esau <gesau@hotmail.co.uk>
2023-11-04 23:53:48 +0000

    Work-around awkward windows path behaviour

--- test.sh
+++ test.sh
@@ -17,6 +17,10 @@ sudo rm -rf temp-persisted-data
 
 echo 'Ensure changes from previous runs can still be loaded (test backward-compatibility)...'
 cp -r test-persisted-data temp-persisted-data
+# Windows doesn't support colons in filenames, so they're checked-in to git with a replacement character (\uf03a).
+# So when running tests on unix-like systems, we need to replace that character back  in file/directory paths.
+export COLON_CHAR="$(printf '\uf03a')"
+find temp-persisted-data -type d -name "*$COLON_CHAR*" | perl -nle "print \$_, ' ', s/$COLON_CHAR/:/rg" | xargs -L 1 mv
 docker compose run --rm test verify
 docker compose stop
 
