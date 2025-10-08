97ae54b60537e574cb61f7918fcc9e066555109b
Graham Esau <gesau@hotmail.co.uk>
2025-10-08 19:01:06 +0100

    Handle `TaggingService`s persisted without the `key_field`/`value_field` properties (#26)
    These properties were only added in v4.3.0, so state persisted from earlier versions will not have them.

--- src/localstack_persist/hooks.py
+++ src/localstack_persist/hooks.py
@@ -1,6 +1,7 @@
 import logging
 
 from localstack.runtime import hooks
+from localstack.utils.tagging import TaggingService
 from moto.core.common_models import CloudFormationModel
 
 from .state import STATE_TRACKER
@@ -12,6 +13,9 @@ LOG = logging.getLogger(__name__)
 def on_infra_start():
     # HACK for "global" models that were persisted without a `partition` field
     setattr(CloudFormationModel, "partition", "aws")
+    # HACK for TaggingServices that were persisted without the `key_field`/`value_field` properties
+    setattr(TaggingService, "key_field", "Key")
+    setattr(TaggingService, "value_field", "Value")
 
     STATE_TRACKER.load_all_services_state()
     STATE_TRACKER.start()
